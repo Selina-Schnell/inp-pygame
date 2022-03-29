41aa6c5a4026b988b5a7c90e0ebe49eec427aa76
Sara <e227742@edu.sbl.ch>
2022-03-29 15:05:14 +0200

    changes

--- src/main.py
+++ src/main.py
@@ -152,7 +152,7 @@ class PlayerSprite(BaseSprite):
                 self.y_velocity = 0
                 self.rect.top = hit.rect.bottom
                 break
-            self.game.playing = FALSE
+            self.game.playing = False
 
 
 
